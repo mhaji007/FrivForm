28ae112d8c8dd7870abb33a935a9f8ae048a6a6a
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-11 18:52:26 -0400

    Render field inputs

---
+++ src/components/Field.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Field extends React.Component {
+    state = {  }
+    render() { 
+        return (
+        <div className="ui field">
+            <label>Name</label>
+            <input/>
+        </div> );
+    }
+}
+ 
+export default Field;
