36bad86f1562c2b4c564aa05435d71447a725fad
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-11 20:23:19 -0400

    Create context object

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import languageContext from '../contexts/languageContext';
 
 class Button extends React.Component {
+
+    static contextType = languageContext;
+
     state = {  }
+    
     render() { 
         return ( 
         <button className="ui button primary">
